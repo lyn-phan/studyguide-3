1285119fdb67cb54470fdc9c70c3b05efdf7f70b
Lynda Phan <lynda.kim.phan@gmail.com>
2021-01-08 03:09:18 +0000

    completed study guide 3

--- coding-practice/server.py
+++ coding-practice/server.py
@@ -7,13 +7,6 @@ app.secret_key = "blahhhhhhhh"
 def show_homepage():
     return render_template('homepage.html')
 
-###############################
-#                             #
-# 1) Finish the routes below. #
-#                             #
-###############################
-
-
 @app.route('/form')
 def show_form():
     
@@ -24,15 +17,42 @@ def show_results():
     
     return render_template('results.html')
 
-@app.route('/save-name', methods=['POST']):
+@app.route('/results', methods=['POST'])
+def receive_results():
+
+    cheery = request.args.get('cheery')
+    honest = request.args.get('honest')
+    dreary = request.args.get('dreary')
+
+    if cheery and honest and dreary:
+        msg = "Do you really?"
+    elif cheery and honest:
+        msg = "You're a happy camper!"
+    elif cheery and dreary:
+        msg = "Well, which one is it?"
+    elif cheery:
+        msg = "Have a jolly good day!"
+    elif honest:
+        msg = "You should really take a shower today."
+    elif dreary:
+        msg = "Tomorrow is going to be just like today."
+    elif honest and dreary:
+        msg = "Are you really in sweats again?"
+    else:
+        msg = "Time to go on a walk"
+    
+    return render_template('/results', msg=msg)
+
+
+@app.route('/save-name', methods=['POST'])
 def save_name():
     """submits user input(name) to this route and adds it to a session w/ key: name
     and redirects to homepage"""
 
-    user = request.form.get('name')
-    saved_user = session[user]
+    name = request.form.get('name')
+    session[name] = name
 
-    return redirect('/')
+    return render_template('homepage.html')
 
 
 if __name__ == "__main__":
